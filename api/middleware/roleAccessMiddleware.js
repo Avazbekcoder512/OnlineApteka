8664aca7a686d345446fad6d2710cfdc9e05be0f
Avazbek Qalandarov <qalandarovavazbek43@gmail.com>
2025-07-03 12:41:05 +0500

    test

---
+++ api/middleware/roleAccessMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken'
+import { JWT_KEY } from '../config/config.js';
+
+const roleAccessMiddleware = function (roles) {
+    return async function (req, res, next) {
+        try {
+            const token = req.cookies.token;
+
+            if (!token) {
+                return res.status(401).send({
+                    success: false,
+                    error: 'Iltimos qayta kirish qiling!'
+                })
+            }
+
+            const { role } = jwt.verify(token, JWT_KEY);
+
+            if (!roles.includes(role)) {
+                return res.status(403).send({
+                    error: "Sizga ruxsat yo'q",
+                });
+            }
+
+            next();
+        } catch (error) {
+            throw error
+        }
+    };
+};
+
+export default roleAccessMiddleware
