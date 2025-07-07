7de02eed7c4e9f47c39b2e1950f14a50d5b1fb88
Avazbek Qalandarov <qalandarovavazbek43@gmail.com>
2025-07-07 09:31:39 +0500

    rollar tekshirildi

--- api/router/pharmacy/pharmacyRouter.js
+++ api/router/pharmacy/pharmacyRouter.js
@@ -1,13 +1,14 @@
 import { Router} from 'express'
 import { deletePharmacy, getAllPharmacies, getOnePharmacy, pharmacyCreate, pharmacyUpdate } from '../../controller/pharmacy.controller.js'
+import roleAccessMiddleware from '../../middleware/roleAccessMiddleware.js'
 
 const router = Router()
 
 router
-.post('/pharmacy/create', pharmacyCreate)
-.get('/pharmacies', getAllPharmacies)
-.get('/pharmacy/:id', getOnePharmacy)
-.put('/pharmacy/:id/update', pharmacyUpdate)
-.delete('/pharmacy/:id/delete', deletePharmacy)
+.post('/pharmacy/create', roleAccessMiddleware(['superAdmin', 'admin']), pharmacyCreate)
+.get('/pharmacies', roleAccessMiddleware(['superAdmin', 'admin']), getAllPharmacies)
+.get('/pharmacy/:id', roleAccessMiddleware(['superAdmin', 'admin']), getOnePharmacy)
+.put('/pharmacy/:id/update', roleAccessMiddleware(['superAdmin', 'admin']), pharmacyUpdate)
+.delete('/pharmacy/:id/delete', roleAccessMiddleware('superAdmin'), deletePharmacy)
 
 export default router
